381fd393cec525efe9527f0bc31007869b55cbb2
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2023-11-22 11:34:40 +0100

    feat: added DTO classes and responses

---
+++ src/main/java/com/chinexboroja/bankapp/dto/BankResponse.java
@@ -0,0 +1,17 @@
+package com.chinexboroja.bankapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BankResponse {
+
+    private String responseCode;
+    private String responseMessage;
+    private AccountInfo accountInfo;
+}
